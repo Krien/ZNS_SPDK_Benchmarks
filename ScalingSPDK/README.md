3a5201dd543c1e0e4aec009b21cfc7ee1413df37
Krijn Doekemeijer <krijn.doekemeijer@gmail.com>
2022-09-29 21:48:19 +0200

    Scaling spdk scripts (#2)
    * Add initial Scaling scripts
    
    * Add ScalingSPDK plot
    
    * Solve shell escape typo
    
    * Add SPDK scaling read scripts
    
    * Patch read commit
    
    * Solve read script resetting zones

--- ScalingSPDK/README.md
+++ ScalingSPDK/README.md
@@ -5,7 +5,8 @@ Try `concurrency_scaling.sh` with a number of devices, it will test how well the
 Data and images will be generated in `/out`. The command I ran was:
 
 ```bash
-sudo ./concurrency_scaling.sh 8 "nvme1n0,1 nvme1n1,1"
+sudo ./concurrency_scaling.sh w 8 "nvme1n0,1 nvme1n1,1"
+sudo ./concurrency_scaling.sh r 8 "nvme1n0,1 nvme1n1,1"
 # and then on a second machine (scp) that comes with Python Matplotlib stuff
 python3 concurrency_scaling_plot.py 8
 ```
--- ScalingSPDK/concurrency_scaling_plot.py
+++ ScalingSPDK/concurrency_scaling_plot.py
@@ -8,31 +8,36 @@ def savefig(fig, name):
     fig.savefig(name + ".png")
     fig.savefig(name + ".svg")
 
-def main(scale):
+def plotscale(scale, label, labelw):
     kiops_libaio = []
     kiops_spdk = []
     for i in range(scale):
-        file_name="./out/concurrency_scaling_write_spdk_" + str(i+1) + ".json"
+        file_name="./out/concurrency_scaling_" + label + "_spdk_" + str(i+1) + ".json"
         file=open(file_name,'r')
         data=file.read()
         data=json.loads(data)
-        kiops_spdk.append(int(data["jobs"][0]["write"]["iops"])/1000)
+        kiops_spdk.append(int(data["jobs"][0][label]["iops"])/1000)
     for i in range(scale):
-        file_name="./out/concurrency_scaling_write_libaio_" + str(i+1) + ".json"
+        file_name="./out/concurrency_scaling_" + label + "_libaio_" + str(i+1) + ".json"
         file=open(file_name,'r')
         data=file.read();
         data=json.loads(data)
-        kiops_libaio.append(data["jobs"][0]["write"]["iops"]/1000)
-
+        kiops_libaio.append(data["jobs"][0][label]["iops"]/1000)
+    
+    fig, ax = plt.subplots()
     plt.plot( [x + 1 for x in list(range(scale))], kiops_libaio, linewidth=3, label='Libaio')
     plt.plot( [x + 1 for x in list(range(scale))], kiops_spdk, linewidth=3, label='SPDK')
     plt.ylim(0, 2*10**2)
     plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
     plt.xlabel("Jobs per SSD")
     plt.ylabel("KIOPS")
-    plt.title("KIOPS: Writing concurrently to 2 ZNS SSDs")
+    plt.title("KIOPS: " + labelw + " concurrently to 2 ZNS SSDs")
     plt.legend()
-    savefig(plt, "out/" + "scale_writes")
+    savefig(plt, "out/" + label)
+
+def main(scale):
+    plotscale(scale, "write", "Writing")
+    plotscale(scale, "read", "Reading")
 
     
 if __name__ == "__main__":
